0e7b6ea35dbfc93405a04d0915d7620edbd6d000
Norris <bedzon@ieee.org>
2022-03-16 08:20:35 -0400

    3. Leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,4 @@
-fndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -12,4 +12,3 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif /* MAIN_H */
-
