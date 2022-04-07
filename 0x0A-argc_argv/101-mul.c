d2e333cd31bac1eada23fc929151e29e5d109288
Norris <99494922+bedzon94@users.noreply.github.com>
2022-04-07 17:38:16 -0400

    Update 101-mul.c

--- 0x0A-argc_argv/101-mul.c
+++ 0x0A-argc_argv/101-mul.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * is_digit - checks for digits
  * @c: input character to check for digit
@@ -12,77 +11,36 @@ int is_digit(char c)
 	if (c >= '0' && c <= '9')
 		return (1);
 	printf("Error\n");
-	return (0);
+	return (98);
 }
 /**
  * multiply - multiplies 2 #'s, prints result, must be 2 #'s
- * @num1: factor # 1 (is the smaller of 2 numbers)
- * @len_1: length of factor 1
- * @num2: factor # 2 (is the larger of 2 numbers)
- * @len_2: length of factor 2
- * @len_r: length of result arrays
+ * @num1: factor # 1 (1st number)
+ * @num2: factor # 2 (2nd number)
  * Return: 0 fail, 1 success
  */
-int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r)
-{
-	int i = 0, i1 = len_1 - 1;
-	int i2, product, carry, digit, *mul_result, *sum_result;
-
-	sum_result = _calloc(sizeof(int), (len_r));
-	while (i < len_1)
-	{
-		mul_result = _calloc(sizeof(int), len_r);
-		i2 = len_2 - 1, digit = (len_r - 1 - i);
-		if (!is_digit(num1[i1]))
-			return (NULL);
-		carry = 0;
-		while (i2 >= 0)
-		{
-			if (!is_digit(num2[i2]))
-				return (NULL);
-			product = (num1[i1] - '0') * (num2[i2] - '0');
-			product += carry;
-			mul_result[digit] += product % 10;
-			carry = product / 10;
-			digit--, i2--;
-		}
-		add_arrays(mul_result, sum_result, len_r);
-		free(mul_result);
-	    i++, i1--;
-	}
-	return (sum_result);
-}
-/**
- * print_me - prints my array of the hopeful product here
- * @sum_result: pointer to int array with numbers to add
- * @len_r: length of result array
- * Return: void
- */
-void print_me(int *sum_result, int len_r)
-{
-	int i = 0;
-
-	while (sum_result[i] == 0 && i < len_r)
-		i++;
-	if (i == len_r)
-		_putchar('0');
-	while (i < len_r)
-		_putchar(sum_result[i++] + '0');
-	_putchar('\n');
-}
+int *multiply(char *num1, char *num2)
 /**
  * main - multiply 2 input #'s and print result or print Error
  * @argc: input count of args
  * @argv: input array of string args
  * Return: 0, Success
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+	char *num1, *num2;
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (1);
+		exit(98);
+		num1 = argv[1], num2 = argv[2];
+	else
+	{
+		num1 = argv[2], num2 = argv[1];
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	sum_result = multiply(num1 * num2);
+	if (sum_result == NULL)
+		exit(98);
+	print_me(sum_result);
 	return (0);
 }
