0411c33a1c71eec9f4ea38c20d4ad6f3b4299aed
Norris <99494922+bedzon94@users.noreply.github.com>
2022-04-07 16:47:54 -0400

    Update 101-mul.c

--- 0x0A-argc_argv/101-mul.c
+++ 0x0A-argc_argv/101-mul.c
@@ -1,198 +1,159 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-void _sum(char **s, unsigned int l);
-char *_multiplyrev(char *s, char c, int o);
-int _isnumber(char *s);
-unsigned int _lenght(char *s);
-void _puts(char *s);
+#include <limits.h>
 
 /**
- * main - check the code for Holberton School students.
- * @argc: Qty of parameters
- * @argv: Array of pointers with the parameters
- *
- * Return: Always 0.
+ * str_len - finds string length
+ * @str: input pointer to string
+ * Return: length of string
+ */
+int str_len(char *str)
+{
+	int len;
+
+	for (len = 0; *str != '\0'; len++)
+		len++, str++;
+	return (len / 2);
+}
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @bytes: bytes of memory needed per size requested
+ * @size: size in bytes of each element
+ * Return: pointer to the allocated memory
+ */
+void *_calloc(unsigned int bytes, unsigned int size)
+{
+	unsigned int i;
+	char *p;
+
+	if (bytes == 0 || size == 0)
+		return (NULL);
+	if (size >= UINT_MAX / bytes || bytes >= UINT_MAX / size)
+		return (NULL);
+	p = malloc(size * bytes);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < bytes * size; i++)
+		p[i] = 0;
+	return ((void *)p);
+}
+/**
+ * add_arrays - adds 2 arrays of ints
+ * @mul_result: pointer to array with numbers from product
+ * @sum_result: pointer to array with numbers from total sum
+ * @len_r: length of both arrays
+ * Return: void
+ */
+void add_arrays(int *mul_result, int *sum_result, int len_r)
+{
+	int i = 0, len_r2 = len_r - 1, carry = 0, sum;
+
+	while (i < len_r)
+	{
+		sum = carry + mul_result[len_r2] + sum_result[len_r2];
+		sum_result[len_r2] = sum % 10;
+		carry = sum / 10;
+		i++;
+		len_r2--;
+	}
+}
+/**
+ * is_digit - checks for digits
+ * @c: input character to check for digit
+ * Return: 0 failure, 1 success
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	printf("Error\n");
+	return (0);
+}
+/**
+ * multiply - multiplies 2 #'s, prints result, must be 2 #'s
+ * @num1: factor # 1 (is the smaller of 2 numbers)
+ * @len_1: length of factor 1
+ * @num2: factor # 2 (is the larger of 2 numbers)
+ * @len_2: length of factor 2
+ * @len_r: length of result arrays
+ * Return: 0 fail, 1 success
+ */
+int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r)
+{
+	int i = 0, i1 = len_1 - 1;
+	int i2, product, carry, digit, *mul_result, *sum_result;
+
+	sum_result = _calloc(sizeof(int), (len_r));
+	while (i < len_1)
+	{
+		mul_result = _calloc(sizeof(int), len_r);
+		i2 = len_2 - 1, digit = (len_r - 1 - i);
+		if (!is_digit(num1[i1]))
+			return (NULL);
+		carry = 0;
+		while (i2 >= 0)
+		{
+			if (!is_digit(num2[i2]))
+				return (NULL);
+			product = (num1[i1] - '0') * (num2[i2] - '0');
+			product += carry;
+			mul_result[digit] += product % 10;
+			carry = product / 10;
+			digit--, i2--;
+		}
+		add_arrays(mul_result, sum_result, len_r);
+		free(mul_result);
+	    i++, i1--;
+	}
+	return (sum_result);
+}
+/**
+ * print_me - prints my array of the hopeful product here
+ * @sum_result: pointer to int array with numbers to add
+ * @len_r: length of result array
+ * Return: void
+ */
+void print_me(int *sum_result, int len_r)
+{
+	int i = 0;
+
+	while (sum_result[i] == 0 && i < len_r)
+		i++;
+	if (i == len_r)
+		_putchar('0');
+	while (i < len_r)
+		_putchar(sum_result[i++] + '0');
+	_putchar('\n');
+}
+/**
+ * main - multiply 2 input #'s of large lengths and print result or print Error
+ * @argc: input count of args
+ * @argv: input array of string args
+ * Return: 0, Success
  */
 int main(int argc, char **argv)
 {
-char **t, *max, *min;
-unsigned int  l, i;
+	int len_1, len_2, len_r, temp, *sum_result;
+	char *num1, *num2;
 
 	if (argc != 3)
 	{
-		_puts("Error\n");
+		printf("Error\n");
 		exit(98);
 	}
-	if (!_isnumber(argv[1]) || !_isnumber(argv[2]))
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-	if (*argv[1] == '0' || *argv[2] == '0')
-	{
-		_puts("0\n");
-		return (0);
-	}
-	if (_lenght(argv[1]) > _lenght(argv[2]))
-	{
-		max = argv[1];
-		min = argv[2];
-		l = _lenght(argv[1]);
-	}
+	len_1 = str_len(argv[1]), len_2 = str_len(argv[2]);
+	len_r = len_1 + len_2;
+	if (len_1 < len_2)
+		num1 = argv[1], num2 = argv[2];
 	else
 	{
-		max = argv[2];
-		min = argv[1];
-		l = _lenght(argv[2]);
+		num1 = argv[2], num2 = argv[1];
+		temp = len_2, len_2 = len_1, len_1 = temp;
 	}
-
-	t = malloc((l + 1) * sizeof(char **));
-	if (t == NULL)
-	{
-		_puts("Error\n");
+	sum_result = multiply(num1, len_1, num2, len_2, len_r);
+	if (sum_result == NULL)
 		exit(98);
-	}
-
-	for (i = 0; i < l; i++)
-	{
-		t[i] = _multiplyrev(min, max[l - i - 1], i);
-	}
-	t[i] = malloc((_lenght(min) + i + 1) * sizeof(char));
-
-	_sum(t, l);
-	_puts(t[i]);
-	_putchar('\n');
-
-	for (i = 0; i <= l; i++)
-		free(t[i]);
-	free(t);
-
+	print_me(sum_result, len_r);
 	return (0);
-}
-
-/**
- * _sum - summ a set of string
- * @s: The set of strings
- * @l: The the lenght of terms
- *
- * Return: None
- */
-void _sum(char **s, unsigned int l)
-{
-char tmp;
-unsigned int  i, j = 0, k = 1;
-unsigned int a, d = 0;
-
-	for (j = 0; k == 1; j++)
-	{
-		a = 0;
-		k = 0;
-		for (i = 0; i < l; i++)
-		{
-			if (j < _lenght(s[i]))
-			{
-				a += (s[i][j] - 48);
-				k = 1;
-			}
-		}
-		if (d <= 0 && k == 0)
-		{
-			s[i][j] = '\0';
-			j--;
-		}
-		else
-			s[i][j] = (char)((a + d) % 10) + 48;
-		d = (a + d) / 10;
-	}
-	j--;
-	for (k = 0; k <= (j / 2); k++)
-	{
-		tmp = s[i][k];
-		s[i][k] = s[i][j - k];
-		s[i][j - k] = tmp;
-	}
-}
-
-/**
- * _multiplyrev - multiplay two strings
- * @s: The first factor
- * @c: The second factor
- * @o: The offset
- *
- * Return: A pointer to the result
- */
-char *_multiplyrev(char *s, char c, int o)
-{
-char *r;
-unsigned int d = 0;
-int  i, j = 0;
-
-	r = malloc((_lenght(s) + o + 2) * sizeof(char));
-	if (r == NULL)
-		return (NULL);
-
-	for (i = 0; i < o; i++)
-		r[i] = '0';
-	for (i = (_lenght(s) - 1); i >= 0; i--)
-	{
-		r[o + j++] = (char)((s[i] - 48) * (c - 48) + d) % 10 + 48;
-		d = ((s[i] - 48) * (c - 48) + d) / 10;
-	}
-	if (d > 0)
-		r[o + j++] = d + 48;
-	r[o + j] = '\0';
-
-	return (r);
-}
-
-
-/**
- * _isnumber - evaluate if a string is a number
- * @s: The string
- *
- * Return: 0 for false or 1 for true.
- */
-int _isnumber(char *s)
-{
-	while (*s != '\0')
-	{
-		if (*s < '0' || *s > '9')
-			return (0);
-		s++;
-	}
-	return (1);
-}
-
-/**
- * _lenght - Find out the lenght of a string
- * @s: The string
- *
- * Return: Lenght.
- */
-unsigned int _lenght(char *s)
-{
-unsigned long i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
- * _puts - Print a string
- * @s: The string
- *
- * Return: Lenght.
- */
-void _puts(char *s)
-{
-unsigned long i = 0;
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
 }
