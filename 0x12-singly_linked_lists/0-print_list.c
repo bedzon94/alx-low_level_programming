07b5dcc784e7e36018fd66e2a75023fda2a63c24
“Norris” <bedzon@ieee.org>
2022-04-21 13:03:50 -0400

    Header file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
@@ -8,12 +7,7 @@
  */
 size_t print_list(const list_t *h)
 {
-<<<<<<< HEAD
-	size_t s = 0;
-
-=======
 	size_t n = 0;
->>>>>>> 01a74b2d7c2d10a788aa5b2307f1991f138c61f1
 	while (h)
 	{
 		if (!h->str)
@@ -21,14 +15,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-<<<<<<< HEAD
-		s++;
-	}
-
-	return (s);
-=======
 		n++;
 	}
 	return (n);
->>>>>>> 01a74b2d7c2d10a788aa5b2307f1991f138c61f1
 }
