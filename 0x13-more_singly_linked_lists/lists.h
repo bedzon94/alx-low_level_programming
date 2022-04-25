b35e7f453969fde075e039a01732e307ec0edb96
“Norris” <bedzon@ieee.org>
2022-04-25 15:55:52 -0400

    Header update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,13 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ * for Holberton project
  */
 typedef struct listint_s
 {
