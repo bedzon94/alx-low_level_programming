07222ae3c67b8b7d79494c3bd626d78350b3da0f
Norris <99494922+bedzon94@users.noreply.github.com>
2022-06-13 12:34:52 -0400

    Win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/bedzon94/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp/ https://github.com/bedzon94/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libwinner.so
+export LD_PRELOAD=/tmp/libwinner.so
